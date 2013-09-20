2d93e68516904eaa5954fc6bb06ab26eaee07133
jy02305022 <fttl_398@126.com>
2013-09-20 15:21:13 +0800

    删除多出来的文件,之前传错了

--- Delegate.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace blqw
-{
-    public delegate object GetJsonValue(object obj);
-    public delegate string GetJsonKey(object obj);
-}
