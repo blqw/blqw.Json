28f4f874f805ce79063c7f63b5d0b886514b899a
jy02305022 <fttl_398@126.com>
2014-08-02 01:47:53 +0800

    增加3个特性类 增加core中的转换类,适应2.0

---
+++ blqw.Json/Attribute/JsonNameAttribute.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace blqw
+{
+    /// <summary> 指示某个字段或属性在Json的序列化和反序列化过程中使用的新名称
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field, Inherited = true, AllowMultiple = false)]
+    public class JsonNameAttribute : Attribute
+    {
+        /// <summary> 指示某个字段或属性在Json的序列化和反序列化过程中使用的新名称
+        /// </summary>
+        /// <param name="name">在Json的序列化和反序列化过程中使用的新名称</param>
+        public JsonNameAttribute(string name)
+        {
+            Name = name;
+        }
+
+        /// <summary> 在Json的序列化和反序列化过程中使用的新名称
+        /// </summary>
+        public string Name { get; private set; }
+    }
+}
---
+++ blqw.Json/Attribute/NonJsonAttribute.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace blqw
+{
+    /// <summary> 指示某个字段或属性不应被序列化成Json。
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field, Inherited = true, AllowMultiple = false)]
+    public class NonJsonAttribute : Attribute
+    {
+        /// <summary> 指示某个字段或属性不应被序列化成Json,但可以从Json中反序列化该成员的值。 
+        /// </summary>
+        public NonJsonAttribute()
+            : this(false)
+        {
+        }
+
+        /// <summary> 指示某个字段或属性不应被序列化成Json,并设置是否可以从Json中反序列化该成员的值。 
+        /// </summary>
+        public NonJsonAttribute(bool nonDeserialize)
+        {
+            NonDeserialize = nonDeserialize;
+        }
+
+        public bool NonDeserialize { get; private set; }
+    }
+}
