2dffddc2c0d22923121c1149c9239c627aa5b951
blqw <fttl_398@126.com>
2014-12-20 22:10:48 +0800

    增加自定义json的转换接口

---
+++ blqw.Json/Interface/IJsonToObject.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace blqw
+{
+    /// <summary> 允许对象控制自己的反序列化行为
+    /// </summary>
+    public interface IJsonToObject
+    {
+        /// <summary> 使用 IJsonObject 的值填充到当前对象中
+        /// </summary>
+        /// <param name="jsonObject">反序列化Json字符串得到的值</param>
+        void Fill(IJsonObject jsonObject);
+    }
+}
